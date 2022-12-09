6ebb55f89c73e006f40c877273cb3b9cf3ae2c1e
Vitor Sanches Ferreira <112482028+SanchesVitor@users.noreply.github.com>
2022-12-09 11:45:09 -0300

    consun api CREATE

---
+++ public/js/api.js
@@ -0,0 +1,8 @@
+let api_key = "247ddcd124bf54c443b8963ff4be2d27"
+
+let img_url = "https://image.tmdb.org/t/p/w500/"
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
+
